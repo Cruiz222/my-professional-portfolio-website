e5b1be5d6f75f63469dcd62f81b2e1754a1a41d9
cruiz222 <destainlex5050@gmail.com>
2026-09-16 23:04:41 +0100

    setting up react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <main>John Abah</main>
+      <h1>Software/AI Engineer</h1>
+
+      <p>I am learning AI and software engineering</p>
+    </div>
+           
+  )
+}
+
+export default App
