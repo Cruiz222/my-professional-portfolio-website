9a68b163cc4c276442b52c836a9cf66baaba5808
cruiz222 <destainlex5050@gmail.com>
2026-09-21 17:08:10 +0100

    building project forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import UpdateTitle from './components/ProjectForm'
 
 function App() {
 
@@ -13,6 +14,8 @@ function App() {
       <About />
       
       <Projects title="My Projects" />
+
+      <UpdateTitle />
     </main>
            
   )
--- frontend/src/components/ProjectForm.tsx
+++ frontend/src/components/ProjectForm.tsx
@@ -5,14 +5,26 @@ function UpdateTitle() {
     
     const [input, setInput] = useState('');
 
+    const [description, setDescription] = useState('');
+
     return (
         <div>
-            <h1>Current Title: {title}</h1>
+            <h1>{title}</h1>
+            <p>{description}</p>
 
-            <input type="text" value="input" onChange={(e) => setInput(e.target.value)} >
-            Input title</input>
+          <label>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            Input title
+            </label> 
 
-            <button onClick={() => setTitle(input)}>Set title</button>
+            <label>
+                <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+                Input description
+            </label>
+
+
+            <button onClick={() => {setTitle(input); setDescription(description)}}>Enter</button>
+
         </div>
 
     )
